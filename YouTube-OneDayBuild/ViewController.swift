fcc16e18463b3c213e0b0a92b7797fe7177d0fad
Miguel <Miguel@ARGENTCODEX.local>
2020-06-15 16:15:35 -0400

    modifying for bitrise fail

--- YouTube-OneDayBuild/ViewController.swift
+++ YouTube-OneDayBuild/ViewController.swift
@@ -14,9 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var i = 0
-        
-        
     }
 
 
